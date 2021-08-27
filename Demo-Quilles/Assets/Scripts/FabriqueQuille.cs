0b5a6bd08c313be4463ec0b24df395197c8dac14
thecakeisreal <36107760+thecakeisreal@users.noreply.github.com>
2021-08-27 16:03:59 -0400

    Premier cours
    Création du projet
    Création de l'allée et des dalots
    Ajout du script FabriqueQuille
    Initialisation des quilles (première étape seulement)

---
+++ Demo-Quilles/Assets/Scripts/FabriqueQuille.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Classe responsable de créer de nouvelles quilles
+public class FabriqueQuille : MonoBehaviour
+{
+    // On crée 10 quilles par défaut
+    public const int NOMBRE_QUILLES = 10;
+
+    // Position de la quille de tête, les autres quilles sont placés derrière (orienté vers le z positif)
+    public Vector3 positionPremiereQuille;
+
+    // Modèle de quille
+    public GameObject prototypeQuille;
+
+    // Référence vers les quilles créées
+    public GameObject[] quilles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreerQuilles();
+    }
+
+    // Crée et dispose un nombre de quilles selon la valeur de la constante NOMBRE_QUILLES
+    public void CreerQuilles()
+    {
+        GameObject quille = Instantiate(prototypeQuille);
+        quille.transform.position = positionPremiereQuille;
+    }
+}
