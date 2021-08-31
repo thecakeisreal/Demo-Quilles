eca579abf57c627e2a224f6532a1382d6c5bcb0d
thecakeisreal <36107760+thecakeisreal@users.noreply.github.com>
2021-08-31 13:37:26 -0400

    Solution placement quilles
    Permet de positionner les 10 quilles (ou n'importe quel nombre de quilles selon la constant NOMBRE_QUILLES

--- Demo-Quilles/Assets/Scripts/FabriqueQuille.cs
+++ Demo-Quilles/Assets/Scripts/FabriqueQuille.cs
@@ -17,16 +17,39 @@ public class FabriqueQuille : MonoBehaviour
     // Référence vers les quilles créées
     public GameObject[] quilles;
 
+    // Valeur pour espacer les quilles
+    public float decalageColonne;
+    public float decalageLigne;
+
     // Start is called before the first frame update
     void Start()
     {
+        quilles = new GameObject[NOMBRE_QUILLES];
+
         CreerQuilles();
     }
 
     // Crée et dispose un nombre de quilles selon la valeur de la constante NOMBRE_QUILLES
     public void CreerQuilles()
     {
-        GameObject quille = Instantiate(prototypeQuille);
-        quille.transform.position = positionPremiereQuille;
+        int ligne = 0, colonne = 0;
+        Vector3 pos = positionPremiereQuille;
+
+        for (int i = 0; i < NOMBRE_QUILLES; i++)
+        {
+            GameObject quille = Instantiate(prototypeQuille);
+            quille.transform.position = pos;
+
+            colonne++;
+            if (colonne > ligne)
+            {
+                ligne++;
+                colonne = 0;
+            }
+
+            // Position de la prochaine quille
+            pos = positionPremiereQuille + new Vector3(colonne * decalageColonne - (ligne % 2) * decalageColonne * 0.5f - 0.5f * (ligne - ligne % 2) * decalageColonne, 0f, ligne * decalageLigne);
+            quilles[i] = quille;
+        }
     }
 }
